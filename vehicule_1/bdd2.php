496ba91a094fb4be8877bc228e5a5af9ba09cd1d
Axel NAY <87807539+AxelNAY@users.noreply.github.com>
2022-05-24 17:12:55 +0200

    Update bdd2.php

--- vehicule_1/bdd2.php
+++ vehicule_1/bdd2.php
@@ -4,7 +4,7 @@ if (isset($_GET['indice'])) { $indice = $_GET['indice']; } //récupération de l
 if (isset($_GET['valeur'])) { $valeur = $_GET['valeur']; } //récupération de la valeur
 //connection à la base de données calene
 	include "connexion.php";
-	$requete = 'SELECT * FROM voiture1 ORDER BY ID_Voiture DESC LIMIT 0,1 ;' ;
+	$requete = 'SELECT * FROM vehicule1 ORDER BY ID_Vehi DESC LIMIT 0,1 ;' ;
 	$resultat = $bdd->query($requete);
 	$compteur = $resultat->rowCount() ;
 	$result = $resultat->fetch() ;
@@ -13,15 +13,15 @@ if (isset($_GET['valeur'])) { $valeur = $_GET['valeur']; } //récupération de l
 	{
 		//conversion de la tension entrée
 		$valeur = $valeur * 10;
-		//itération permettant de choisir le champ (Tension_1 ou Tension_2)
+		//itération permettant de choisir le champ (Volt_1 ou Volt_2)
 		if ($indice == '1')
 		{
-			$requete1 = "INSERT INTO voiture1 (Commentaire, Courant_1, Courant_2, Tension_1, Tension_2, Vitesse, Latitude, Longitude)
+			$requete1 = "INSERT INTO vehicule1 (Message, Amp_1, Amp_2, Volt_1, Volt_2, Speed, Lat, Long)
 			VALUES ('$result[2]', '$result[3]','$result[4]','$valeur','$result[6]','$result[7]','$result[8]','$result[9]')";
 		}
 		else if ($indice == '2')
 		{
-			$requete1 = "INSERT INTO voiture1 (Commentaire, Courant_1, Courant_2, Tension_1, Tension_2, Vitesse, Latitude, Longitude)
+			$requete1 = "INSERT INTO vehicule1 (Message, Amp_1, Amp_2, Volt_1, Volt_2, Speed, Lat, Long)
 			VALUES ('$result[2]', '$result[3]','$result[4]','$result[5]','$valeur','$result[7]','$result[8]','$result[9]')";
 		}
 	}
@@ -34,15 +34,15 @@ if (isset($_GET['valeur'])) { $valeur = $_GET['valeur']; } //récupération de l
 		{
 			$valeur = $valeur + 65536;
 		}
-		//itération permettant de choisir le champ (Courant_1 ou Courant_2)
+		//itération permettant de choisir le champ (Amp_1 ou Amp_2)
 		if ($indice == '1')
 		{
-			$requete1 = "INSERT INTO voiture1 (Commentaire, Courant_1, Courant_2, Tension_1, Tension_2, Vitesse, Latitude, Longitude)
+			$requete1 = "INSERT INTO vehicule1 (Message, Amp_1, Amp_2, Volt_1, Volt_2, Speed, Lat, Long)
 			VALUES ('$result[2]', '$valeur','$result[4]','$result[5]','$result[6]','$result[7]','$result[8]','$result[9]')";
 		}
 		else if ($indice == '2')
 		{
-			$requete1 = "INSERT INTO voiture1 (Commentaire, Courant_1, Courant_2, Tension_1, Tension_2, Vitesse, Latitude, Longitude)
+			$requete1 = "INSERT INTO vehicule1 (Message, Amp_1, Amp_2, Volt_1, Volt_2, Speed, Lat, Long)
 			VALUES ('$result[2]', '$result[3]','$valeur','$result[5]','$result[6]','$result[7]','$result[8]','$result[9]')";
 		}
 	}
@@ -50,7 +50,7 @@ if (isset($_GET['valeur'])) { $valeur = $_GET['valeur']; } //récupération de l
 	{
 		//conversion de la vitesse entrée
 		$valeur = $valeur / 3.6;
-		$requete1 = "INSERT INTO voiture1 (Commentaire, Courant_1, Courant_2, Tension_1, Tension_2, Vitesse, Latitude, Longitude)
+		$requete1 = "INSERT INTO vehicule1 (Message, Amp_1, Amp_2, Volt_1, Volt_2, Speed, Lat, Long)
 		VALUES ('$result[2]', '$result[3]','$result[4]','$result[5]','$result[6]','$valeur','$result[8]','$result[9]')";
 	}
 	$resultat = $bdd->query($requete1);
