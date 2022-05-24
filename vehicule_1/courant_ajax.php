02db91dc38d31da680c6ce3cc183c74bae28ed7b
Axel NAY <87807539+AxelNAY@users.noreply.github.com>
2022-05-24 20:13:56 +0200

    Update courant_ajax.php

--- vehicule_1/courant_ajax.php
+++ vehicule_1/courant_ajax.php
@@ -3,7 +3,7 @@
 	include "connexion.php";
 //Récupération de la dernière valeur enregistrée dans les champs Amp_1 et Amp_2 de la table vehicule 1
 header("Content-type: text/json");
-	$reponse = $bdd->query('SELECT Amp_1, Amp_2 FROM vehicule2 ORDER BY ID_Vehi DESC LIMIT 0,1');
+	$reponse = $bdd->query('SELECT Amp_1, Amp_2 FROM vehicule1 ORDER BY ID_Vehi DESC LIMIT 0,1');
 	while ( $donnees = $reponse->fetch() ) {
 		echo "[" ;
 		echo $donnees['0'] ;
