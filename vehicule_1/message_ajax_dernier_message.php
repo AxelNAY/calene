8589b48c58b6ec199e6dea3c327d46d489fb903e
Axel NAY <87807539+AxelNAY@users.noreply.github.com>
2022-05-24 19:14:03 +0200

    Update message_ajax_dernier_message.php

--- vehicule_1/message_ajax_dernier_message.php
+++ vehicule_1/message_ajax_dernier_message.php
@@ -11,7 +11,7 @@ die('Erreur : ' . $e->getMessage());
 // Set the JSON header
 header("Content-type: text/json");
 
-	$reponse = $bdd->query('SELECT Message FROM vehicule_1 ORDER BY ID DESC LIMIT 0,1');
+	$reponse = $bdd->query('SELECT Message FROM vehicule1 ORDER BY ID DESC LIMIT 0,1');
 	while ( $donnees = $reponse->fetch() ) {
 		echo "[" ;
 		echo $donnees['0'] ;
