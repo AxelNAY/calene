ba933b1db77e3f0af9a631a3d6fc56d968d383f2
Axel NAY <87807539+AxelNAY@users.noreply.github.com>
2022-05-24 18:28:11 +0200

    Update bdd2.php

--- vehicule_2/bdd2.php
+++ vehicule_2/bdd2.php
@@ -11,11 +11,11 @@ echo "<BR>" ;
 echo $valeur ;
 echo "<BR>" ;
 
-//connection à la base de données Calene
+//connection à la base de données Projet_BTS
 	include "connexion.php";
 
-	$requete = 'SELECT * FROM voiture2 ORDER BY ID_Voiture DESC LIMIT 0,1 ;' ;
-	//$requete = "SELECT * FROM voiture2 ;" ;
+	$requete = 'SELECT * FROM vehicule2 ORDER BY ID_Vehi DESC LIMIT 0,1 ;' ;
+	//$requete = "SELECT * FROM vehicule2 ;" ;
 	echo $requete ;
 	echo "<BR>" ;
 	$resultat = $bdd->query($requete);
@@ -55,14 +55,16 @@ echo "<BR>" ;
 	{
 		//conversion de la tension entrée
 		$valeur = $valeur * 10;
-		//itération permettant de choisir le champ (Tension_1 ou Tension_2)
+		//itération permettant de choisir le champ (Volt_1 ou Volt_2)
 		if ($indice == '1')
 		{
-			$requete2 = "INSERT INTO voiture2 (Commentaire, Courant_1, Courant_2, Tension_1, Tension_2, Vitesse, Latitude, Longitude) VALUES ('$result[2]', '$result[3]','$result[4]','$valeur','$result[6]','$result[7]','$result[8]','$result[9]')";
+			$requete2 = "INSERT INTO vehicule2 (Message, Amp_1, Amp_2, Volt_1, Volt_2, Speed, Lat, Long)
+			VALUES ('$result[2]', '$result[3]','$result[4]','$valeur','$result[6]','$result[7]','$result[8]','$result[9]')";
 		}
 		else if ($indice == '2')
 		{
-			$requete2 = "INSERT INTO voiture2 (Commentaire, Courant_1, Courant_2, Tension_1, Tension_2, Vitesse, Latitude, Longitude) VALUES ('$result[2]', '$result[3]','$result[4]','$result[5]','$valeur','$result[7]','$result[8]','$result[9]')";
+			$requete2 = "INSERT INTO vehicule2 (Message, Amp_1, Amp_2, Volt_1, Volt_2, Speed, Lat, Long)
+			VALUES ('$result[2]', '$result[3]','$result[4]','$result[5]','$valeur','$result[7]','$result[8]','$result[9]')";
 		}
 	}
 	if ($type == 'I')
@@ -74,22 +76,25 @@ echo "<BR>" ;
 		{
 			$valeur = $valeur + 65536;
 		}
-		//itération permettant de choisir le champ (Courant_1 ou Courant_2)
+		//itération permettant de choisir le champ (Amp_1 ou Amp_2)
 		if ($indice == '1')
 		{
-			$requete2 = "INSERT INTO voiture2 (Commentaire, Courant_1, Courant_2, Tension_1, Tension_2, Vitesse, Latitude, Longitude) VALUES ('$result[2]', '$valeur','$result[4]','$result[5]','$result[6]','$result[7]','$result[8]','$result[9]')";
+			$requete2 = "INSERT INTO vehicule2 (Message, Amp_1, Amp_2, Volt_1, Volt_2, Speed, Lat, Long)
+			VALUES ('$result[2]', '$valeur','$result[4]','$result[5]','$result[6]','$result[7]','$result[8]','$result[9]')";
 			echo $requete ;
 		}
 		else if ($indice == '2')
 		{
-			$requete2 = "INSERT INTO voiture2 (Commentaire, Courant_1, Courant_2, Tension_1, Tension_2, Vitesse, Latitude, Longitude) VALUES ('$result[2]', '$result[3]','$valeur','$result[5]','$result[6]','$result[7]','$result[8]','$result[9]')";
+			$requete2 = "INSERT INTO vehicule2 (Message, Amp_1, Amp_2, Volt_1, Volt_2, Speed, Lat, Long)
+			VALUES ('$result[2]', '$result[3]','$valeur','$result[5]','$result[6]','$result[7]','$result[8]','$result[9]')";
 		}
 	}
 	if ($type == 'V')
 	{
 		//conversion de la vitesse entrée
 		$valeur = $valeur / 3.6;
-		$requete2 = "INSERT INTO voiture2 (Commentaire, Courant_1, Courant_2, Tension_1, Tension_2, Vitesse, Latitude, Longitude) VALUES ('$result[2]', '$result[3]','$result[4]','$result[5]','$result[6]','$valeur','$result[8]','$result[9]')";
+		$requete2 = "INSERT INTO vehicule2 (Message, Amp_1, Amp_2, Volt_1, Volt_2, Speed, Lat, Long)
+		VALUES ('$result[2]', '$result[3]','$result[4]','$result[5]','$result[6]','$valeur','$result[8]','$result[9]')";
 	}
 	echo $requete2 ;
 	echo "<BR>" ;
