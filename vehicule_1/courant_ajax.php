05b9580cd10f3ba2ef1eb2113bbc5c06c9d19bb3
Axel NAY <87807539+AxelNAY@users.noreply.github.com>
2022-05-24 19:07:28 +0200

    Update courant_ajax.php

--- vehicule_1/courant_ajax.php
+++ vehicule_1/courant_ajax.php
@@ -1,7 +1,7 @@
 <?php
-//connection à la base de données Calene
+//connection à la base de données Projet_BTS
 	include "connexion.php";
-//Récupération de la dernière valeur enregistrée dans les champs Amp_1 et Amp_2 de la table vehicule 2
+//Récupération de la dernière valeur enregistrée dans les champs Amp_1 et Amp_2 de la table vehicule 1
 header("Content-type: text/json");
 	$reponse = $bdd->query('SELECT Amp_1, Amp_2 FROM vehicule2 ORDER BY ID_Vehi DESC LIMIT 0,1');
 	while ( $donnees = $reponse->fetch() ) {
