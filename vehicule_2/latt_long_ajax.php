3981ca0093f0dc6bbf4ed1b8e1bf85add0aa6a66
Axel NAY <87807539+AxelNAY@users.noreply.github.com>
2022-05-24 18:42:39 +0200

    Update latt_long_ajax.php

--- vehicule_2/latt_long_ajax.php
+++ vehicule_2/latt_long_ajax.php
@@ -1,10 +1,10 @@
 <?php
-//connection à la base de données Calene
+//connection à la base de données Projet_BTS
 	include "connexion.php";
 // Set the JSON header
 header("Content-type: text/json");
 
-	$reponse = $bdd->query('SELECT Latitude, Longitude FROM voiture2 ORDER BY ID_Voiture DESC LIMIT 0,1');
+	$reponse = $bdd->query('SELECT Lat, Long FROM vehicule2 ORDER BY ID_Vehi DESC LIMIT 0,1');
 	while ( $donnees = $reponse->fetch() ) {
 		echo "[" ;
 		echo $donnees['0'] ;
