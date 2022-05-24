b4e56777c9c955f71d843a9e0864fea2495d5b74
Axel NAY <87807539+AxelNAY@users.noreply.github.com>
2022-05-24 17:17:31 +0200

    Update contact_ajax.php

--- vehicule_1/contact_ajax.php
+++ vehicule_1/contact_ajax.php
@@ -5,12 +5,12 @@ $message = str_replace("'", "\'", $message);
 //connection à la base de données Calene
 include "connexion.php";
 //requête permettant de récupérer le dernier enregistrement
-	$requete = 'SELECT * FROM voiture1 ORDER BY ID_Voiture DESC LIMIT 0,1 ;' ;
+	$requete = 'SELECT * FROM vehicule1 ORDER BY ID_Vehi DESC LIMIT 0,1 ;' ;
 	$resultat = $bdd->query($requete);
 	$compteur = $resultat->rowCount() ;
 	$result = $resultat->fetch() ;
 //requête permettant d'entrer le commentaire dans la base de données calene
-$requete1 = "INSERT INTO voiture1 (Commentaire, Courant_1, Courant_2, Tension_1, Tension_2, Vitesse, Latitude, Longitude)
+$requete1 = "INSERT INTO vehicule1 (Message, Amp_1, Amp_2, Volt_1, Volt_2, Speed, Lat, Long)
 VALUES ('$message','$result[3]', '$result[4]','$result[5]','$result[6]','$result[7]','$result[8]','$result[9]')";
 $resultat = $bdd->query($requete1);
 $compteur1 = $resultat->rowCount() ; // on compte le nombre de résultat à notre requete : 1 si ok sinon 0
