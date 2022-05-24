2d21b362a8dfc4e64473e654bdcc559115b3b6b3
Axel NAY <87807539+AxelNAY@users.noreply.github.com>
2022-05-24 22:12:11 +0200

    Update tension_ajax.php

--- vehicule_2/tension_ajax.php
+++ vehicule_2/tension_ajax.php
@@ -1,9 +1,9 @@
 <?php
-//connection à la base de données Calene
+//connection à la base de données Projet_BTS
 	include "connexion.php";
-//Récupération de la dernière valeur enregistrée dans les champs Tension_1 et Tension_2 de la table voiture 2
+//Récupération de la dernière valeur enregistrée dans les champs Volt_1 et Volt_2 de la table vehicule 2
 header("Content-type: text/json");
-	$reponse = $bdd->query('SELECT Tension_1,Tension_2 FROM voiture2 ORDER BY ID_Voiture DESC LIMIT 0,1');
+	$reponse = $bdd->query('SELECT Volt_1, Volt_2 FROM vehicule2 ORDER BY ID_Vehi DESC LIMIT 0,1');
 	while ( $donnees = $reponse->fetch() ) {
 		echo "[" ;
 		echo $donnees['0'] ;
