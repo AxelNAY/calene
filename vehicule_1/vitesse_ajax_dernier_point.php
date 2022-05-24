b0d4c272095261446be6a01c4de92a346c834c50
Axel NAY <87807539+AxelNAY@users.noreply.github.com>
2022-05-24 18:20:58 +0200

    Update vitesse_ajax_dernier_point.php

--- vehicule_1/vitesse_ajax_dernier_point.php
+++ vehicule_1/vitesse_ajax_dernier_point.php
@@ -2,8 +2,8 @@
 //connection à la base de données Calene
 	include "connexion.php";
 	header("Content-type: text/json");
-//Récupération de la dernière valeur enregistrée dans les champs Temps et Vitesse de la table voiture 1
-	$requete = 'SELECT Temps, Vitesse FROM voiture1 ORDER BY ID_Voiture DESC LIMIT 0,1' ;
+//Récupération de la dernière valeur enregistrée dans les champs Time et Speed de la table vehicule 1
+	$requete = 'SELECT Time, Speed FROM vehicule1 ORDER BY ID_Vehi DESC LIMIT 0,1' ;
 	$reponse = $bdd->query($requete);
 	while ( $donnees = $reponse->fetch() ) {
 		echo "[" ;
