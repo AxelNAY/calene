c6a0a5fe85272e8f2b00a0e3ec1921a268dde174
Axel NAY <87807539+AxelNAY@users.noreply.github.com>
2022-05-24 17:27:56 +0200

    Update login_ajax.php

--- vehicule_1/login_ajax.php
+++ vehicule_1/login_ajax.php
@@ -1,9 +1,9 @@
 <?php
-//connection à la base de données calene
+//connection à la base de données Projet_BTS
 	include "connexion.php";
 	$user = $_POST['username'] ;
 	$password = $_POST['password'] ;
-	$requete = "SELECT * FROM Autorisation WHERE utilisateur='".$user."' AND motdepasse='".$password."'" ; 
+	$requete = "SELECT * FROM Autorisation WHERE user='".$user."' AND motdepasse='".$password."'" ; 
 	$resultat = $bdd->query($requete);
 	$compteur = $resultat->rowCount() ; // on compte le nombre de résultat à notre requete : 1 si ok sinon 0
 	if ($compteur == 1)
