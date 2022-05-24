cb7e3cf9d6a38540b107e8b1aefdea176c119690
Axel NAY <87807539+AxelNAY@users.noreply.github.com>
2022-05-24 18:57:51 +0200

    Update contact_ajax.php

--- vehicule_1/contact_ajax.php
+++ vehicule_1/contact_ajax.php
@@ -2,14 +2,14 @@
 //récupération du message
 $message = $_POST['comments'];
 $message = str_replace("'", "\'", $message);
-//connection à la base de données Calene
+//connection à la base de données Projet_BTS
 include "connexion.php";
 //requête permettant de récupérer le dernier enregistrement
 	$requete = 'SELECT * FROM vehicule1 ORDER BY ID_Vehi DESC LIMIT 0,1 ;' ;
 	$resultat = $bdd->query($requete);
 	$compteur = $resultat->rowCount() ;
 	$result = $resultat->fetch() ;
-//requête permettant d'entrer le commentaire dans la base de données calene
+//requête permettant d'entrer le commentaire dans la base de données Projet_BTS
 $requete1 = "INSERT INTO vehicule1 (Message, Amp_1, Amp_2, Volt_1, Volt_2, Speed, Lat, Long)
 VALUES ('$message','$result[3]', '$result[4]','$result[5]','$result[6]','$result[7]','$result[8]','$result[9]')";
 $resultat = $bdd->query($requete1);
