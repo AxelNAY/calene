85c2c3bdea232f8bbaa6bf3d8c7481e1cd1e24d1
Axel NAY <87807539+AxelNAY@users.noreply.github.com>
2022-05-24 18:03:46 +0200

    Update message_ajax_dernier_message.php

--- vehicule_1/message_ajax_dernier_message.php
+++ vehicule_1/message_ajax_dernier_message.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-$bdd = new PDO('mysql:host=cprp.lolacie.fr;dbname=cprp;charset=utf8', 'etudiant.cprp', 'etudiant47');
+$bdd = new PDO('mysql:host=cprp.lolacie.fr;dbname=Projet_BTS;charset=utf8', 'identifiant', 'motdepasse');
 }
 catch (Exception $e)
 {
@@ -11,7 +11,7 @@ die('Erreur : ' . $e->getMessage());
 // Set the JSON header
 header("Content-type: text/json");
 
-	$reponse = $bdd->query('SELECT Message FROM voiture_1 ORDER BY ID DESC LIMIT 0,1');
+	$reponse = $bdd->query('SELECT Message FROM vehicule_1 ORDER BY ID DESC LIMIT 0,1');
 	while ( $donnees = $reponse->fetch() ) {
 		echo "[" ;
 		echo $donnees['0'] ;
